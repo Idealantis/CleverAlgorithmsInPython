bd5e768374d2996481c56289ed44c1305d3d56d3
Harish Allakonda <ahari884@gmail.com>
2017-09-14 05:08:19 +0530

    NeuralAlogrithms/Perceptron added

---
+++ src/NeuralAlgorithms/Perceptron.py
@@ -0,0 +1,52 @@
+import numpy as np
+# NAND, NOR, OR and AND
+def activate(weights, pattern):
+    sum = weights[len(weights)-1]*1.0
+    for index, element in enumerate(pattern):
+        sum += weights[index]*element
+    return sum
+def updateWeights(inputSize, weights, pattern, expected, output, learningRate):
+    #print inputSize, weights, pattern, expected, output, learningRate
+    for index in range(inputSize):
+        weights[index] += learningRate*(expected-output)*pattern[index]
+    weights[inputSize] += learningRate*(expected-output)*1.0
+    return weights
+def getOutput(weights, pattern):
+    activation = activate(weights, pattern)
+    return 1.0 if activation >= 0 else 0.0
+def train(weights, trainInput, trainExpected, inputSize, epochs, learningRate):
+    for epoch in range(epochs):
+        print "Epoch #"+str(epoch)
+        error = 0.0
+        print(trainInput)
+        for i, pattern in enumerate(trainInput):
+            pattern = pattern.astype(float)
+            output = getOutput(weights, pattern)
+            expected = float(trainExpected[i])
+            error += np.absolute(output - expected)
+            print(inputSize)
+            weights = updateWeights(inputSize, weights, pattern, expected, output, learningRate)
+        print "Weights",weights
+        print "Error rate :",error
+    return weights
+def test(weights, testInput, testExpected, learningRate):
+    accuracy = 0
+    for index, pattern in enumerate(testInput):
+        output = getOutput(weights, pattern)
+        if output == testExpected[index]:
+            accuracy += 1
+    return (accuracy*100.0)/len(testInput)
+
+def perceptron(trainData, inputSize, epochs=20, learningRate=0.01):
+    trainData = np.array(trainData)
+    # divide the train data as input and output
+    trainInput = trainData[:,:inputSize]
+    trainExpected = trainData[:,inputSize]
+    # Initialize weights randomly
+    weights = np.random.uniform(-1,1,inputSize+1)
+    # train weights with trainData
+    weights = train(weights, trainInput, trainExpected, inputSize, epochs, learningRate)
+    # In our case testData and trainData are same
+    testInput, testExpected = trainInput, trainExpected
+    accuracy = test(weights, testInput, testExpected, learningRate)
+    return list(weights), accuracy
---
+++ src/Tests/NeuralTestSuite.py
@@ -0,0 +1,23 @@
+'''
+Created on Sep 14, 2017
+
+@author: Sai Panyam
+
+Unit tests that exercise the algorithms. It is a mixture of both real 'unit' tests and functional tests of search
+'''
+class TestNeuralAlgorithms:
+    def test_Perceptron(self):
+        from NeuralAlgorithms.Perceptron import perceptron
+        #algorithm configuration
+        trainData = [[1,0,1],[0,1,1], [1,0,1], [1,1,0]]
+        inputSize=2
+        epochs=20000
+        learningRate=0.01
+
+        weights, accuracy = perceptron(trainData, inputSize, epochs, learningRate)
+        print("*"*30)
+        print("")
+        print("Weights:",weights)
+        print("Accuracy :",accuracy)
+        print("")
+        print("*"*30)
