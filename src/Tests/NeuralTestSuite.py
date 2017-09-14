14206b3569596796a5848a088e73667822df7cc7
Natte Sharan <nattesharan@gmail.com>
2017-09-14 18:05:26 +0530

    Backpropagation done

--- src/Tests/NeuralTestSuite.py
+++ src/Tests/NeuralTestSuite.py
@@ -13,7 +13,6 @@ class TestNeuralAlgorithms:
         inputSize=2
         epochs=20
         learningRate=0.01
-
         weights, accuracy = perceptron(trainData, inputSize, epochs, learningRate)
         print("*"*30)
         print("")
@@ -21,3 +20,19 @@ class TestNeuralAlgorithms:
         print("Accuracy :",accuracy)
         print("")
         print("*"*30)
+    def test_Backpropagation(self):
+        from NeuralAlgorithms.Backpropagation import createBPN,trainBPN,predict
+        trainset = [[0,0,0],[0,1,1],[1,0,1],[1,1,0]]
+        lrate = 0.5
+        epoch = 150
+        nInputs = len(trainset[0]) - 1
+        nOutputs = len(set([row[-1] for row in trainset]))
+        network = createBPN(nInputs, 2, nOutputs)
+        trainBPN(network,trainset,lrate,epoch,nOutputs)
+        for layer in network:
+            print(layer)
+        print('Complted the training')
+        testset = [[0,0,0],[0,1,1],[1,0,1]]
+        for row in testset:
+            prediction = predict(network, row)
+            print('Expected=%d, Got=%d' % (row[-1], prediction))
